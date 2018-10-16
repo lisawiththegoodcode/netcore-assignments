c33d646604ec884b267556cd650eaaeee027d8f0
lisa umhey <lisa.umhey@gmail.com>
2018-10-16 17:51:32 -0500

    lesson 2 prework complete with posting data

--- lesson02/prework/ToDo/Controllers/StatusController.cs
+++ lesson02/prework/ToDo/Controllers/StatusController.cs
@@ -24,10 +24,45 @@ namespace ToDoApp.Controllers
             return View();
         }
 
+        // POST: Status/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Status collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
         // GET: Status/Edit
         public ActionResult Edit(int id)
         {
-            return View((Repository.Statuses.FirstOrDefault(x => x.Key == id).Value));
+            return View((Repository.Statuses.FirstOrDefault(x => x.Key == id)).Value);
+        }
+
+        // POST: Status/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Status collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }

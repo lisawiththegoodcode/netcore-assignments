92c8304a184572bfe36f4c419aea646209b3162e
lisa umhey <lisa.umhey@gmail.com>
2018-10-18 14:28:34 -0500

    taghelper written, need some help with understanding where the input is supposed to be passed

---
+++ lesson02/part1-homework/TagHelpers/InputTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpers
+{
+    class InputTagHelper : TagHelper
+    {
+        public string AutocompleteStatus { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+
+            if (AutocompleteStatus == "off" || AutocompleteStatus == "false" || AutocompleteStatus == "disabled")
+            {
+                output.Attributes.SetAttribute("autocomplete", "off");
+
+            }
+            else
+            {
+                output.Attributes.SetAttribute("autocomplete", "on");
+            }
+
+        }
+
+    }
+}

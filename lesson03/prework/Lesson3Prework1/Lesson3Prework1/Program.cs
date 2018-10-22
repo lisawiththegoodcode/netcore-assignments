05f8fa9a768eeea95ab37bd89b59567fe3cf1890
lisa umhey <lisa.umhey@gmail.com>
2018-10-22 10:21:15 -0500

    lesson3 part 1 complete

---
+++ lesson03/prework/Lesson3Prework1/Lesson3Prework1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lesson3Prework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Starting");
+            System.Threading.Tasks.Task.Run(() =>
+            {
+                System.Console.WriteLine("Hello World!");
+                System.Threading.Tasks.Task.Delay(2000).Wait();
+                System.Console.WriteLine("Hello Again!");
+            });
+            System.Console.WriteLine("Ending");
+            System.Console.ReadKey();
+
+        }
+    }
+}

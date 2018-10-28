379d6cce3595de02dcb45c06254c0f7e480a0bc9
lisawiththegoodcode <lisa.umhey@gmail.com>
2018-10-28 14:59:49 -0500

    checkpoint 1 initial commit

---
+++ checkpoint1/AppointmentTracker/AppointmentTracker/Controllers/AppointmentController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentTracker.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        // GET: Appointment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Appointment/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Appointment/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Appointment/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Appointment/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Appointment/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Appointment/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Appointment/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

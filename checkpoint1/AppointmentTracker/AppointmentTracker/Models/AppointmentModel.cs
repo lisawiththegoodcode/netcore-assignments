55a66ddbf999c1564af37e1a6b065eb89cfb0871
lisawiththegoodcode <lisa.umhey@gmail.com>
2018-10-31 19:43:13 -0500

    working on display and editor templates

--- checkpoint1/AppointmentTracker/AppointmentTracker/Models/AppointmentModel.cs
+++ checkpoint1/AppointmentTracker/AppointmentTracker/Models/AppointmentModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,17 @@ namespace AppointmentTracker.Models
     public class AppointmentModel
     {
         public int Id { get; set; }
+
         public DateTime AppointmentTime { get; set; }
+
         public int CustomerId { get; set; }
+        [UIHint("Client)")]
         public CustomerModel Client { get; set; }
+
         public int ServiceProviderId { get; set; }
+        [UIHint("Provider")]
         public ServiceProviderModel Provider { get; set; }
+
         public string Service { get; set; }
 
     }
